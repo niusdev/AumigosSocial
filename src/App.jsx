dc984f480beca33c3e965c49184aab8b1537d73c
niusdev <vinidamascenadev@gmail.com>
2026-03-18 22:31:38 -0300

    feat: create initial project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Login from "./components/Login/Login";
+import './App.css'
 function App() {
-  return(
-    <div>App React</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
