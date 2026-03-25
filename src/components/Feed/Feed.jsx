3c4b029cf4ceb54013252f4425ff0a18131de22d
niusdev <vinidamascenadev@gmail.com>
2026-03-25 16:14:22 -0300

    feat: create initial Feed

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhotos from "./FeedPhotos";
 
 const Feed = () => {
   return (
     <div>
-      Feed
+      <FeedModal />
+      <FeedPhotos />
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
