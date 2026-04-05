a2075ac03fa2e8dec7d808f555cb06e4fe76d998
niusdev <vinidamascenadev@gmail.com>
2026-04-05 12:31:19 -0300

    feat: create user profile page and error page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import "./App.css";
 import UserStorage from "./UserStorage";
 import ProtectedRoute from "./components/Helpers/ProtectedRoute";
 import Photo from "./components/Photo/Photo";
+import UserProfile from "./components/User/UserProfile";
+import NotFound from "./components/NotFound";
 function App() {
   return (
     <div>
@@ -27,6 +29,8 @@ function App() {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -5,17 +5,19 @@ import Feed from "../Feed/Feed";
 import UserPhotoPost from "./UserPhotoPost";
 import UserStats from "./UserStats";
 import { UserContext } from "../../UserContext";
+import NotFound from "../NotFound";
 
 const User = () => {
-  const {data} = useContext(UserContext);
+  const { data } = useContext(UserContext);
 
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed user={data.id}/>}/>
-        <Route path="postar" element={<UserPhotoPost />}/>
-        <Route path="estatisticas" element={<UserStats />}/>
+        <Route path="/" element={<Feed user={data.id} />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
   );
