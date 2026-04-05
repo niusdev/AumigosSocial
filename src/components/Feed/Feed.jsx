e48c279facdc53a0b9cd1a45c2a7c7404bcf1fda
niusdev <vinidamascenadev@gmail.com>
2026-04-05 14:21:04 -0300

    chore: add proptypes to the feed component

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
+import PropTypes from "prop-types";
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
@@ -47,8 +48,30 @@ const Feed = ({ user }) => {
             setInfinite={setInfinite}
           />
         ))}
+      {!infinite && !user && (
+        <p
+          style={{
+            textAlign: "center",
+            padding: "2rem 0 4rem 0",
+            color: "#888",
+          }}
+        >
+          Não há mais postagens.
+        </p>
+      )}
     </div>
   );
+};
+
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
 };
 
 export default Feed;
